2517f7e3c39a0e9f11c319b16e9acb4aa34997bb
Victor <stalin1945@gmail.com>
2011-05-18 00:40:27 +0300

    Finished: login coding and font embedding Added: jQuery libraries Added: Scrollable plugin Added: jQuery Form plugin

--- application/modules/admin/controllers/UsersController.php
+++ application/modules/admin/controllers/UsersController.php
@@ -11,12 +11,70 @@ class Admin_UsersController extends Zend_Controller_Action {
 	}
 
 	public function loginAction() {
-		// action body
+		$request = $this->getRequest();
+
+		$loginForm = new Admin_Form_Login(array(
+			'name' => 'loginForm',
+			'class' => 'login_box',
+			'action' => $this->_helper->url('login')
+		));
+
+		$notEmptyValidator = new Zend_Validate_NotEmpty();
+		$notEmptyValidator->setMessage('Email & Password are Required', Zend_Validate_NotEmpty::IS_EMPTY);
+
+		$loginForm->email->addValidator($notEmptyValidator, true);
+		$loginForm->password->addValidator($notEmptyValidator, true);
+
+		if ($request->isPost()) {
+			if ($loginForm->isValid($request->getPost())) {
+				$user = Service_User::create(array(
+					'email' => $loginForm->email->getValue(),
+					'password' => $loginForm->password->getValue()
+				));
+				$auth = Zend_Auth::getInstance();
+				$authResult = $auth->authenticate($user);
+				if ($authResult->isValid()) {
+					$user->lastLoginDate = time();
+					$user->save();
+					if (!$request->isXmlHttpRequest()) {
+						$referer = $loginForm->referer->getValue();
+						$this->_redirect(($referer) ? $referer : '/' . $request->getModuleName());
+					}
+				}
+				else {
+					$this->view->error = array(
+						'form' => $authResult->getMessages()
+					);
+				}
+			}
+			else {
+				$errors = $loginForm->getMessages();
+				if (isset($errors['email']['isEmpty']) && isset($errors['password']['isEmpty'])) {
+					unset($errors['password']['isEmpty']);
+					if (count($errors['password']) == 0) {
+						unset($errors['password']);
+					}
+				}
+				$this->view->error = $errors;
+			}
+		}
+		else {
+			Zend_Layout::getMvcInstance()->setLayout('login', true);
+			$forgotForm = new Admin_Form_ForgotPassword(array(
+				'name' => 'forgotForm',
+				'action' => $this->_helper->url('forget')
+			));
+
+			$loginForm->referer->setValue($request->getRequestUri());
+
+			$this->view->loginForm = $loginForm->prepareDecorators();
+			$this->view->forgotForm = $forgotForm->prepareDecorators();
+		}
 	}
 
 	public function logoutAction() {
-		// action body
+		Zend_Auth::getInstance()->clearIdentity();
+		$this->_redirect('/' . $this->getRequest()->getModuleName());
 	}
-
 
 }
